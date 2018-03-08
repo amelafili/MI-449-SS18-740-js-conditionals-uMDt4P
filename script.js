841e0615615e339fdfef19066b8990c9d53222d2
amelafili <31477792+amelafili@users.noreply.github.com>
2018-03-08 15:05:29 -0500

    added first question with if, else if, and else, and used confirm for second message

---
+++ script.js
@@ -0,0 +1,14 @@
+var firstMessage = window.prompt('Hey! Do you like turtles?')
+
+if (firstMessage === 'yes') {
+  window.alert('Cool, I love turtles!')
+} else if (firstMessage === 'no') {
+  window.alert('That\'s sad to hear. :(')
+} else {
+  window.alert('That doesn\'t make sense!')
+}
+
+var secondMessage = (confirm('Can we keep going?'))
+if (secondMessage) {
+  
+}
